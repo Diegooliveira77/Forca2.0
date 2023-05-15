ab2f3e4424a45f2a999972cbe8801953ee77c792
Diego <Diego@DESKTOP-VKT3JJP>
2023-05-15 09:02:58 -0300

    Add project files.

---
+++ Forca2.0WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace Forca2._0WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbPalavra = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbTentadas = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbFaltam = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbErros = new System.Windows.Forms.Label();
+            this.tbPalavra = new System.Windows.Forms.TextBox();
+            this.btComeca = new System.Windows.Forms.Button();
+            this.btTenta = new System.Windows.Forms.Button();
+            this.mtLetra = new System.Windows.Forms.MaskedTextBox();
+            this.pbBracoD = new System.Windows.Forms.PictureBox();
+            this.pbPernaE = new System.Windows.Forms.PictureBox();
+            this.pbBracoE = new System.Windows.Forms.PictureBox();
+            this.pbCorpo = new System.Windows.Forms.PictureBox();
+            this.pbCabeca = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbPernaD = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBracoD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPernaE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBracoE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCorpo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCabeca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPernaD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jogo da Forca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "digite a palavra";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Digite uma letra";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Palavra";
+            // 
+            // lbPalavra
+            // 
+            this.lbPalavra.AutoSize = true;
+            this.lbPalavra.Location = new System.Drawing.Point(107, 204);
+            this.lbPalavra.Name = "lbPalavra";
+            this.lbPalavra.Size = new System.Drawing.Size(13, 13);
+            this.lbPalavra.TabIndex = 4;
+            this.lbPalavra.Text = "_";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tentativas";
+            // 
+            // lbTentadas
+            // 
+            this.lbTentadas.AutoSize = true;
+            this.lbTentadas.Location = new System.Drawing.Point(107, 240);
+            this.lbTentadas.Name = "lbTentadas";
+            this.lbTentadas.Size = new System.Drawing.Size(13, 13);
+            this.lbTentadas.TabIndex = 6;
+            this.lbTentadas.Text = "_";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(45, 303);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Faltam";
+            // 
+            // lbFaltam
+            // 
+            this.lbFaltam.AutoSize = true;
+            this.lbFaltam.Location = new System.Drawing.Point(107, 303);
+            this.lbFaltam.Name = "lbFaltam";
+            this.lbFaltam.Size = new System.Drawing.Size(13, 13);
+            this.lbFaltam.TabIndex = 8;
+            this.lbFaltam.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(52, 358);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Erros";
+            // 
+            // lbErros
+            // 
+            this.lbErros.AutoSize = true;
+            this.lbErros.Location = new System.Drawing.Point(107, 358);
+            this.lbErros.Name = "lbErros";
+            this.lbErros.Size = new System.Drawing.Size(13, 13);
+            this.lbErros.TabIndex = 10;
+            this.lbErros.Text = "0";
+            // 
+            // tbPalavra
+            // 
+            this.tbPalavra.Location = new System.Drawing.Point(119, 71);
+            this.tbPalavra.Name = "tbPalavra";
+            this.tbPalavra.PasswordChar = '*';
+            this.tbPalavra.Size = new System.Drawing.Size(100, 20);
+            this.tbPalavra.TabIndex = 12;
+            // 
+            // btComeca
+            // 
+            this.btComeca.Location = new System.Drawing.Point(290, 71);
+            this.btComeca.Name = "btComeca";
+            this.btComeca.Size = new System.Drawing.Size(75, 23);
+            this.btComeca.TabIndex = 13;
+            this.btComeca.Text = "Começar";
+            this.btComeca.UseVisualStyleBackColor = true;
+            this.btComeca.Click += new System.EventHandler(this.btComeca_Click);
+            // 
+            // btTenta
+            // 
+            this.btTenta.Enabled = false;
+            this.btTenta.Location = new System.Drawing.Point(290, 125);
+            this.btTenta.Name = "btTenta";
+            this.btTenta.Size = new System.Drawing.Size(75, 23);
+            this.btTenta.TabIndex = 14;
+            this.btTenta.Text = "Tentar";
+            this.btTenta.UseVisualStyleBackColor = true;
+            this.btTenta.Click += new System.EventHandler(this.btTenta_Click);
+            // 
+            // mtLetra
+            // 
+            this.mtLetra.Enabled = false;
+            this.mtLetra.Location = new System.Drawing.Point(119, 125);
+            this.mtLetra.Mask = "a";
+            this.mtLetra.Name = "mtLetra";
+            this.mtLetra.Size = new System.Drawing.Size(100, 20);
+            this.mtLetra.TabIndex = 15;
+            // 
+            // pbBracoD
+            // 
+            this.pbBracoD.Enabled = false;
+            this.pbBracoD.Image = global::Forca2._0WindowsFormsApp1.Properties.Resources.perna_2;
+            this.pbBracoD.Location = new System.Drawing.Point(546, 204);
+            this.pbBracoD.Name = "pbBracoD";
+            this.pbBracoD.Size = new System.Drawing.Size(46, 35);
+            this.pbBracoD.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBracoD.TabIndex = 21;
+            this.pbBracoD.TabStop = false;
+            this.pbBracoD.Visible = false;
+            // 
+            // pbPernaE
+            // 
+            this.pbPernaE.Enabled = false;
+            this.pbPernaE.Image = global::Forca2._0WindowsFormsApp1.Properties.Resources.perna;
+            this.pbPernaE.Location = new System.Drawing.Point(479, 266);
+            this.pbPernaE.Name = "pbPernaE";
+            this.pbPernaE.Size = new System.Drawing.Size(49, 50);
+            this.pbPernaE.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPernaE.TabIndex = 20;
+            this.pbPernaE.TabStop = false;
+            this.pbPernaE.Visible = false;
+            // 
+            // pbBracoE
+            // 
+            this.pbBracoE.Enabled = false;
+            this.pbBracoE.Image = global::Forca2._0WindowsFormsApp1.Properties.Resources.perna;
+            this.pbBracoE.Location = new System.Drawing.Point(492, 204);
+            this.pbBracoE.Name = "pbBracoE";
+            this.pbBracoE.Size = new System.Drawing.Size(36, 35);
+            this.pbBracoE.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBracoE.TabIndex = 19;
+            this.pbBracoE.TabStop = false;
+            this.pbBracoE.Visible = false;
+            // 
+            // pbCorpo
+            // 
+            this.pbCorpo.Enabled = false;
+            this.pbCorpo.Image = global::Forca2._0WindowsFormsApp1.Properties.Resources.corpo_da_forca;
+            this.pbCorpo.Location = new System.Drawing.Point(518, 194);
+            this.pbCorpo.Name = "pbCorpo";
+            this.pbCorpo.Size = new System.Drawing.Size(37, 79);
+            this.pbCorpo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCorpo.TabIndex = 18;
+            this.pbCorpo.TabStop = false;
+            this.pbCorpo.Visible = false;
+            // 
+            // pbCabeca
+            // 
+            this.pbCabeca.Enabled = false;
+            this.pbCabeca.Image = global::Forca2._0WindowsFormsApp1.Properties.Resources.cabeça_da_forca1;
+            this.pbCabeca.Location = new System.Drawing.Point(492, 135);
+            this.pbCabeca.Name = "pbCabeca";
+            this.pbCabeca.Size = new System.Drawing.Size(100, 61);
+            this.pbCabeca.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCabeca.TabIndex = 17;
+            this.pbCabeca.TabStop = false;
+            this.pbCabeca.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Forca2._0WindowsFormsApp1.Properties.Resources.vazia;
+            this.pictureBox1.Location = new System.Drawing.Point(393, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 354);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbPernaD
+            // 
+            this.pbPernaD.Enabled = false;
+            this.pbPernaD.Image = global::Forca2._0WindowsFormsApp1.Properties.Resources.perna_2;
+            this.pbPernaD.Location = new System.Drawing.Point(544, 266);
+            this.pbPernaD.Name = "pbPernaD";
+            this.pbPernaD.Size = new System.Drawing.Size(48, 50);
+            this.pbPernaD.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPernaD.TabIndex = 22;
+            this.pbPernaD.TabStop = false;
+            this.pbPernaD.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 475);
+            this.Controls.Add(this.pbPernaD);
+            this.Controls.Add(this.pbBracoD);
+            this.Controls.Add(this.pbPernaE);
+            this.Controls.Add(this.pbBracoE);
+            this.Controls.Add(this.pbCorpo);
+            this.Controls.Add(this.pbCabeca);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.mtLetra);
+            this.Controls.Add(this.btTenta);
+            this.Controls.Add(this.btComeca);
+            this.Controls.Add(this.tbPalavra);
+            this.Controls.Add(this.lbErros);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lbFaltam);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbTentadas);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbPalavra);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Jogo da Forca";
+            ((System.ComponentModel.ISupportInitialize)(this.pbBracoD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPernaE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBracoE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCorpo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCabeca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPernaD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbPalavra;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbTentadas;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbFaltam;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbErros;
+        private System.Windows.Forms.TextBox tbPalavra;
+        private System.Windows.Forms.Button btComeca;
+        private System.Windows.Forms.Button btTenta;
+        private System.Windows.Forms.MaskedTextBox mtLetra;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbCabeca;
+        private System.Windows.Forms.PictureBox pbCorpo;
+        private System.Windows.Forms.PictureBox pbBracoE;
+        private System.Windows.Forms.PictureBox pbPernaE;
+        private System.Windows.Forms.PictureBox pbBracoD;
+        private System.Windows.Forms.PictureBox pbPernaD;
+    }
+}
+
---
+++ Forca2.0WindowsFormsApp1/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forca2._0WindowsFormsApp1
+{
+    
+    public partial class Form1 : Form
+    {
+        string Palavra, Tentadas;
+        char Letras;
+        int Quantidade = 0, Erros = 0  , Faltam = 0;
+        bool achou = false ,  tenta = false ;
+       
+        private void btTenta_Click(object sender, EventArgs e)
+        {
+            // verifica se a letra foi achada 
+
+            Letras = mtLetra.Text[0];
+            for (int cont = 0; cont != Quantidade; cont++)
+            {
+                if (Letras == Escondido[cont])
+                {
+                    tenta = true; 
+                }
+                //verifica se a letra já foi tentanda 
+
+                Tentadas = lbTentadas.Text;
+                int quant = Tentadas.Length;
+                for ( cont=0; cont!= quant; cont++)
+                {
+                    if (Letras == Tentadas[cont])
+                    {
+                         tenta = true;
+                    }
+                    if (tenta == true)
+                    {
+                        MessageBox.Show(" Vc ja digitou essa letra ");
+                    }
+                    else
+                    {
+                        for (cont = 0; cont != quant; cont++)
+                        {
+                            if (Letras == Palavra[cont])
+                            {
+                                Escondido[cont] = Letras;
+                                achou = true;
+                                Faltam = Faltam - 1;
+                            }
+                        }
+                    }
+                    lbPalavra.Text = "";
+                    for ( cont= 0; cont != Quantidade; cont++)
+                    {
+                        lbPalavra.Text = lbPalavra.Text + Escondido[cont];
+                    }
+                    if (Faltam== 0)
+                    {
+                        MessageBox.Show("Parabéns vc Venceu");
+                        tbPalavra.Enabled = true;
+                        btComeca.Enabled = true;
+                        mtLetra.Enabled = false;
+                        btTenta.Enabled = false;
+                        tbPalavra.Text = "";
+                        tbPalavra.Focus();
+
+                    }
+                    if ((achou==false) & (tenta == false))
+                    {
+                        Erros  = Erros + 1;
+                        lbTentadas.Text = lbTentadas.Text + " " + Letras;
+                    }
+                    //codigo que desenha o boneco 
+                    if (Erros ==1)
+                    { pbCabeca.Visible = true; }
+                    if (Erros ==2)
+                    { pbCorpo.Visible = true; }
+                    if(Erros ==3)
+                    { pbBracoE.Visible = true; }
+                    if (Erros == 4)
+                    { pbBracoD.Visible = true; }                    
+                    if(Erros == 5)
+                    {pbPernaD.Visible = true; }
+                    if (Erros == 6) 
+                    {
+                        pbBracoE.Visible = true;
+                        MessageBox.Show("Vc Perder");
+                        tbPalavra.Enabled = true;
+                        btComeca.Enabled=true;
+                        mtLetra.Enabled=false;
+                        btTenta.Enabled=false;
+                        tbPalavra.Text = "";
+                        tbPalavra.Focus();
+
+                    }
+                    //reinicializando 
+                    tenta = false;
+                    achou = false;
+                    mtLetra.Text = "";
+                    mtLetra.Focus();
+
+                    lbFaltam.Text = Faltam.ToString();
+                    lbErros.Text = Erros.ToString();
+
+                }
+            }
+        }
+
+        char[] Escondido;
+        public Form1()
+        {
+            InitializeComponent();
+            Escondido = new char[20];
+        }
+
+        private void btComeca_Click(object sender, EventArgs e)
+        {
+            Palavra = tbPalavra.Text;
+            Quantidade = Palavra.Length;
+            Faltam = Quantidade;
+            lbPalavra.Text ="";
+            lbTentadas.Text = "";
+
+            for (int cont = 0; cont !=  Quantidade;  cont++)
+            {
+                Escondido[cont] = '*';
+                lbPalavra.Text = lbPalavra.Text + Escondido [cont];
+            }
+            Erros = 0;
+
+            pbCabeca.Visible = false;   
+            pbBracoE.Visible = false;
+            pbBracoD.Visible = false;
+            pbPernaD.Visible = false;
+            pbPernaE.Visible = false;
+            pbCorpo.Visible = false;
+
+            tbPalavra.Enabled = false;
+            btComeca.Enabled = false;
+            mtLetra.Enabled = true;
+            btTenta.Enabled = true;
+
+            mtLetra.Focus();
+            lbFaltam.Text=Faltam.ToString();
+            lbErros.Text= Erros.ToString();
+        }
+    }
+}
